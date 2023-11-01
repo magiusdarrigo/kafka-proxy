a95b5adf94df9ea1014eb723e01b8ff119644f04
Matteo Agius <magiusdarrigo@gmail.com>
2023-11-01 10:13:02 -0500

    modified jwt plugin to just read from file

--- cmd/plugin-unsecured-jwt-info/main.go
+++ cmd/plugin-unsecured-jwt-info/main.go
@@ -1,160 +1,45 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"encoding/base64"
-	"encoding/json"
-	"errors"
-	"flag"
-	"os"
-	"strings"
-	"time"
-
 	"github.com/grepplabs/kafka-proxy/pkg/apis"
-	"github.com/grepplabs/kafka-proxy/pkg/libs/util"
-	"github.com/grepplabs/kafka-proxy/plugin/token-info/shared"
+	"github.com/grepplabs/kafka-proxy/plugin/token-provider/shared"
 	"github.com/hashicorp/go-plugin"
 	"github.com/sirupsen/logrus"
+	"os"
 )
 
 const (
-	StatusOK                      = 0
-	StatusEmptyToken              = 1
-	StatusParseJWTFailed          = 2
-	StatusWrongAlgorithm          = 3
-	StatusUnauthorized            = 4
-	StatusNoIssueTimeInToken      = 5
-	StatusNoExpirationTimeInToken = 6
-	StatusTokenTooEarly           = 7
-	StatusTokenExpired            = 8
-
-	AlgorithmNone = "none"
+	StatusOK            = 0
+	StatusEncodeError   = 1
+	StatusFileReadError = 2
+	jwtFilePath         = "/var/run/secrets/tokens/service-token"
 )
 
-var (
-	clockSkew = 1 * time.Minute
-)
+type UnsecuredJWTProvider struct{}
 
-type UnsecuredJWTVerifier struct {
-	claimSub  map[string]struct{}
-	algorithm map[string]struct{}
-}
-
-type pluginMeta struct {
-	claimSub  util.ArrayFlags
-	algorithm util.ArrayFlags
-}
-
-func (f *pluginMeta) flagSet() *flag.FlagSet {
-	fs := flag.NewFlagSet("unsecured-jwt-info info settings", flag.ContinueOnError)
-	fs.Var(&f.claimSub, "claim-sub", "Allowed subject claim (user name)")
-	fs.Var(&f.algorithm, "algorithm", "Allowed algorithm")
-	return fs
-}
-
-// Implements apis.TokenInfo
-func (v UnsecuredJWTVerifier) VerifyToken(ctx context.Context, request apis.VerifyRequest) (apis.VerifyResponse, error) {
-	if request.Token == "" {
-		return getVerifyResponseResponse(StatusEmptyToken)
-	}
-
-	header, claimSet, err := Decode(request.Token)
+func (v UnsecuredJWTProvider) GetToken(ctx context.Context, request apis.TokenRequest) (apis.TokenResponse, error) {
+	token, err := os.ReadFile(jwtFilePath)
 	if err != nil {
-		return getVerifyResponseResponse(StatusParseJWTFailed)
+		logrus.Errorf("Error reading JWT from file: %v", err)
+		return getGetTokenResponse(StatusFileReadError, "")
 	}
-	if len(v.algorithm) != 0 {
-		if _, ok := v.algorithm[header.Algorithm]; !ok {
-			return getVerifyResponseResponse(StatusUnauthorized)
-		}
-	}
-	if len(v.claimSub) != 0 {
-		if _, ok := v.claimSub[claimSet.Sub]; !ok {
-			return getVerifyResponseResponse(StatusUnauthorized)
-		}
-	}
-	if claimSet.Iat < 1 {
-		return getVerifyResponseResponse(StatusNoIssueTimeInToken)
-	}
-	if claimSet.Exp < 1 {
-		return getVerifyResponseResponse(StatusNoExpirationTimeInToken)
-	}
-
-	earliest := int64(claimSet.Iat) - int64(clockSkew.Seconds())
-	latest := int64(claimSet.Exp) + int64(clockSkew.Seconds())
-	unix := time.Now().Unix()
-
-	if unix < earliest {
-		return getVerifyResponseResponse(StatusTokenTooEarly)
-	}
-	if unix > latest {
-		return getVerifyResponseResponse(StatusTokenExpired)
-	}
-	return getVerifyResponseResponse(StatusOK)
+	return getGetTokenResponse(StatusOK, string(token))
 }
 
-type Header struct {
-	Algorithm string `json:"alg"`
-}
-
-// kafka client sends float instead of int
-type ClaimSet struct {
-	Sub         string                 `json:"sub,omitempty"`
-	Exp         float64                `json:"exp"`
-	Iat         float64                `json:"iat"`
-	OtherClaims map[string]interface{} `json:"-"`
-}
-
-func Decode(token string) (*Header, *ClaimSet, error) {
-	args := strings.Split(token, ".")
-	if len(args) < 2 {
-		return nil, nil, errors.New("jws: invalid token received")
-	}
-	decodedHeader, err := base64.RawURLEncoding.DecodeString(args[0])
-	if err != nil {
-		return nil, nil, err
-	}
-	decodedPayload, err := base64.RawURLEncoding.DecodeString(args[1])
-	if err != nil {
-		return nil, nil, err
-	}
-
-	header := &Header{}
-	err = json.NewDecoder(bytes.NewBuffer(decodedHeader)).Decode(header)
-	if err != nil {
-		return nil, nil, err
-	}
-	claimSet := &ClaimSet{}
-	err = json.NewDecoder(bytes.NewBuffer(decodedPayload)).Decode(claimSet)
-	if err != nil {
-		return nil, nil, err
-	}
-	return header, claimSet, nil
-}
-
-func getVerifyResponseResponse(status int) (apis.VerifyResponse, error) {
+func getGetTokenResponse(status int, token string) (apis.TokenResponse, error) {
 	success := status == StatusOK
-	return apis.VerifyResponse{Success: success, Status: int32(status)}, nil
+	return apis.TokenResponse{Success: success, Status: int32(status), Token: token}, nil
 }
 
 func main() {
-	pluginMeta := &pluginMeta{}
-	fs := pluginMeta.flagSet()
-	_ = fs.Parse(os.Args[1:])
-
-	logrus.Infof("Unsecured JWT sub claims: %v", pluginMeta.claimSub)
-
-	unsecuredJWTVerifier := &UnsecuredJWTVerifier{
-		claimSub:  pluginMeta.claimSub.AsMap(),
-		algorithm: pluginMeta.algorithm.AsMap(),
-	}
+	unsecuredJWTProvider := &UnsecuredJWTProvider{}
 
 	plugin.Serve(&plugin.ServeConfig{
 		HandshakeConfig: shared.Handshake,
 		Plugins: map[string]plugin.Plugin{
-			"unsecuredJWTInfo": &shared.TokenInfoPlugin{Impl: unsecuredJWTVerifier},
+			"unsecuredJWTProvider": &shared.TokenProviderPlugin{Impl: unsecuredJWTProvider},
 		},
-		// A non-nil value here enables gRPC serving for this plugin...
 		GRPCServer: plugin.DefaultGRPCServer,
 	})
 }
